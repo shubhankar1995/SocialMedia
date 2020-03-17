06dfa4540c0821ec2042a397554e1f9dd017c697
Shubhankar <shubhankar1995@gmail.com>
2020-03-17 18:50:57 +1100

    parameter passing in url

--- socialmedia/src/pages/ArticlePage.js
+++ socialmedia/src/pages/ArticlePage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const ArticlePage = () => (
-    <React.Fragment>
-        <h1>This is an article</h1>
-        
-    </React.Fragment>
+const ArticlePage = ({ match }) => {
 
-);
+    const name = match.params.name;
+
+    return (
+        <React.Fragment>
+            <h1>This is the {name} article</h1>
+            
+        </React.Fragment>
+
+    );
+}
 
 export default ArticlePage;
