e92e1f6f14b4abc42f434d7f222e5c1f7ce10188
Shubhankar <shubhankar1995@gmail.com>
2020-03-17 17:31:48 +1100

    added nav bar on all pages and enabled links

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -6,13 +6,15 @@ import{
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
-import ArticlesList from './pages/ArticlesList'
+import ArticlesList from './pages/ArticlesList';
+import NavBar from './NavBar';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact/>
           <Route path="/about" component={AboutPage} />
