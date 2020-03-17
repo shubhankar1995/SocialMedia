505423f6916e5b9614b4408dd86e6b93ff28df57
Shubhankar <shubhankar1995@gmail.com>
2020-03-18 00:30:37 +1100

    configure nodemon

--- socialmedia-backend/src/server.js
+++ socialmedia-backend/src/server.js
@@ -21,7 +21,7 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
 
     articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
 })
 
 app.listen(8000, () => console.log('Listening on port 8000'));
