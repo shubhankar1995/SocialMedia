daae2b4e0746d4735df1e43562f54c4c3685fedc
Shubhankar <shubhankar1995@gmail.com>
2020-03-17 20:28:09 +1100

    implementing 404:Page Not Found

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import{
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 import './App.css';
 
 function App() {
@@ -16,10 +18,13 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact/>
-          <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesListPage} />
-          <Route path="/article/:name" component={ArticlePage} />
+          <Switch>
+            <Route path="/" component={HomePage} exact/>
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component = {NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
--- socialmedia/src/pages/ArticlePage.js
+++ socialmedia/src/pages/ArticlePage.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import ArticlesList from '../components/ArticleList'
+import React from 'react';
+import ArticlesList from '../components/ArticleList';
 import articleContent from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
 
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
-    if (!article) return <h1>{name} Article does not exist</h1>
+    if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name != name);
 
@@ -19,7 +20,7 @@ const ArticlePage = ({ match }) => {
             ))}
 
             <h3>Other Articles</h3>
-            
+
             <ArticlesList articles={otherArticles} />
         </React.Fragment>
 
