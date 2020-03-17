e1013a017dd11ef2b9d7d13fcef3a04cb984c704
Shubhankar <shubhankar1995@gmail.com>
2020-03-17 16:35:25 +1100

    first execution of basic page

---
+++ socialmedia/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello, welcome to my social media site</h1>
+        <p>
+            Welcome to this page and happy sharing
+        </p>
+        <p>
+            I'm still welcoming you
+        </p>
+    </React.Fragment>
+
+);
+
+export default HomePage;
