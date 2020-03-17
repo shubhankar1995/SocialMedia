4d2f49e20310ce197329cd7f92fea48365b944b5
Shubhankar <shubhankar1995@gmail.com>
2020-03-17 19:04:44 +1100

    parameter passing in url and displaying article

--- socialmedia/src/pages/ArticlePage.js
+++ socialmedia/src/pages/ArticlePage.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
 
     const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
 
+    if (!article) return <h1>{name} Article does not exist</h1>
     return (
         <React.Fragment>
-            <h1>This is the {name} article</h1>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph,key )=> (
+                <p key = {key}>{paragraph}</p>
+            ))}
             
         </React.Fragment>
 
