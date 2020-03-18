2f48770d88de85ed8f5cadd5cd44dbe9b609aa68
Shubhankar <shubhankar1995@gmail.com>
2020-03-18 15:05:08 +1100

    Rewritting the upvote endpoint

--- socialmedia-backend/src/server.js
+++ socialmedia-backend/src/server.js
@@ -25,11 +25,29 @@ app.get('/api/articles/:name', async (req, res) => {
     
 });
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
+app.post('/api/articles/:name/upvote', async (req, res) => {
 
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+    try{
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser : true});
+        const db = client.db('my-blog');
+
+        const articleInfo  = await db.collection('articles').findOne({ name: articleName});
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({name:articleName});
+
+        res.status(200).json(updatedArticleInfo);
+
+        client.close()
+        
+    }catch(error){
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }    
 });
 
 app.post('/api/articles/:name/add-comment', (req,res) => {
